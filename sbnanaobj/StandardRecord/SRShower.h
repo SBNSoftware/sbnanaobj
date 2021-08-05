f4576a50abb4af19a45152cb8ba699546092fa54
Christopher Backhouse <bckhouse@fnal.gov>
2021-08-05 09:13:33 -0500

    Clean up _planeN variables and just use the vectors.

--- sbnanaobj/StandardRecord/SRShower.h
+++ sbnanaobj/StandardRecord/SRShower.h
@@ -26,20 +26,10 @@ namespace caf
       float density;                ///< shower density [MeV/cm]
       float len;                    ///< shower length [cm]
       float open_angle;             ///< shower opening angle [rad]
-      float dEdx_plane0;            ///< shower calculated dEdx at each plane [MeV/cm]
-      float dEdx_plane1;            ///< shower calculated dEdx at each plane [MeV/cm]
-      float dEdx_plane2;            ///< shower calculated dEdx at each plane [MeV/cm]
-      float energy_plane0;          ///< shower calculated energy at each plane [GeV]
-      float energy_plane1;          ///< shower calculated energy at each plane [GeV]
-      float energy_plane2;          ///< shower calculated energy at each plane [GeV]
-      unsigned int nHits_plane0;    ///< Number of hits associated to the shower
-      unsigned int nHits_plane1;    ///< Number of hits associated to the shower
-      unsigned int nHits_plane2;    ///< Number of hits associated to the shower
-      float wirePitch_plane0;       ///< Wire pitch corrected for the angle of the shower [cm]
-      float wirePitch_plane1;       ///< Wire pitch corrected for the angle of the shower [cm]
-      float wirePitch_plane2;       ///< Wire pitch corrected for the angle of the shower [cm]
       std::vector<float> dEdx;      ///< shower calculated dEdx at each plane [MeV/cm]
       std::vector<float> energy;    ///< shower calculated energy at each plane [GeV]
+      std::vector<unsigned int> nHits; ///< Number of hits associated to the shower in each plane
+      std::vector<float> wirePitch; ///< Wire pitch corrected for the angle of the shower in each plane [cm]
       SRVector3D dir;               ///< direction cosines at the start of the shower
       SRVector3D start;             ///< shower start point in detector coordinates [cm]
       SRVector3D end;               ///< shower end point (start+len*dir) in detector coordinates [cm]
