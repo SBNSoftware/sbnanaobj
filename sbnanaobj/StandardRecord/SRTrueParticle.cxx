8bf8aa483a34b29f297d1676f2c7f2d97fdbc650
Edward Tyley <e.tyley@sheffield.ac.uk>
2021-10-19 09:28:05 -0500

    Remove functions from SRTrueParticles

--- sbnanaobj/StandardRecord/SRTrueParticle.cxx
+++ sbnanaobj/StandardRecord/SRTrueParticle.cxx
@@ -36,33 +36,5 @@ namespace caf
     gstatus(kIStUndefined)
   {
   }
-
-  bool SRTrueParticle::IsPrimary() const {
-    return start_process == kG4primary;
-  }
-
-  bool SRTrueParticle::HasBraggPeak() const {
-    return \
-      // check contained, id, & end process (stopping-only)
-      contained &&
-      ( abs(pdg) == 13 || abs(pdg) == 2212 ||
-  abs(pdg) == 211 || abs(pdg) == 321 ) &&
-      ( end_process ==  kG4CoupledTransportation ||
-  end_process ==  kG4FastScintillation ||
-  end_process ==  kG4Decay ||
-  end_process ==  kG4muMinusCaptureAtRest );
-  }
-
-  bool SRTrueParticle::IsGenie() const {
-    return gstatus !=  kNotGenie;
-  }
-
-  bool SRTrueParticle::IsStable() const {
-    return \
-       gstatus ==  kNotGenie // non-genie particles are stable
-    || gstatus ==  kIStStableFinalState; // stable genie particle
-
-  }
-
 } // end namespace caf
 ////////////////////////////////////////////////////////////////////////
