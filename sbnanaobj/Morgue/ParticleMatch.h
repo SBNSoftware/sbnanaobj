df71ccb3a48f3721799b717b147e0e4913fc12e2
Henry Lay <h.lay@lancaster.ac.uk>
2021-11-03 12:44:43 -0500

    Remove comments in Morgue product

--- sbnanaobj/Morgue/ParticleMatch.h
+++ sbnanaobj/Morgue/ParticleMatch.h
@@ -6,12 +6,12 @@
 
 namespace caf
 {
-  /// Match from a reconstructed track to a true particle  */
+  /* Deprecated Class */
   class ParticleMatch {
   public:
-    ParticleMatch(); //!< Constructor
-    int G4ID; //!< ID of the particle match, taken from G4 */
-    float energy; //!< Total energy matching between reco track and true particle across all three planes [GeV]. NOTE: this energy is a sum of the depoisted energy as seen individually by each plane */
+    ParticleMatch();
+    int G4ID; 
+    float energy;
   };
 } // end namespace
 
