b6b289f6fff274ba1375fdc3e4f7cfac2f1e903f
Bruce Howard <howard@fnal.gov>
2022-02-16 14:04:59 -0600

    Switch to using braces

--- sbnanaobj/StandardRecord/SRNuID.h
+++ sbnanaobj/StandardRecord/SRNuID.h
@@ -16,16 +16,16 @@ namespace caf
   public:
     SRNuID();
 
-    float nufspfos         = kSignalingNaN; //!< NuNFinalStatePfos feature in NeutrinoID
-    int   nutothits        = -1;            //!< NuNHitsTotal feature in NeutrinoID
-    float nuvtxy           = kSignalingNaN; //!< NuVertexY feature in NeutrinoID
-    float nuwgtdirz        = kSignalingNaN; //!< NuWeightedDirZ feature in NeutrinoID
-    float nusps            = kSignalingNaN; //!< NuNSpacePointsInSphere feature in NeutrinoID
-    float nueigen          = kSignalingNaN; //!< NuEigenRatioInSphere feature in NeutrinoID
-    float crlongtrkdiry    = kSignalingNaN; //!< CRLongestTrackDirY feature in NeutrinoID
-    float crlongtrkdef     = kSignalingNaN; //!< CRLongestTrackDeflection feature in NeutrinoID
-    float crlongtrkhitfrac = kSignalingNaN; //!< CRFracHitsInLongestTrack feature in NeutrinoID
-    int   crmaxhits        = -1;            //!< CRNHitsMax feature in NeutrinoID
+    float nufspfos         { kSignalingNaN }; //!< NuNFinalStatePfos feature in NeutrinoID
+    int   nutothits        { -1 };            //!< NuNHitsTotal feature in NeutrinoID
+    float nuvtxy           { kSignalingNaN }; //!< NuVertexY feature in NeutrinoID
+    float nuwgtdirz        { kSignalingNaN }; //!< NuWeightedDirZ feature in NeutrinoID
+    float nusps            { kSignalingNaN }; //!< NuNSpacePointsInSphere feature in NeutrinoID
+    float nueigen          { kSignalingNaN }; //!< NuEigenRatioInSphere feature in NeutrinoID
+    float crlongtrkdiry    { kSignalingNaN }; //!< CRLongestTrackDirY feature in NeutrinoID
+    float crlongtrkdef     { kSignalingNaN }; //!< CRLongestTrackDeflection feature in NeutrinoID
+    float crlongtrkhitfrac { kSignalingNaN }; //!< CRFracHitsInLongestTrack feature in NeutrinoID
+    int   crmaxhits        { -1 };            //!< CRNHitsMax feature in NeutrinoID
 
     void setDefault();
   };
