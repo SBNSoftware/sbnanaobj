b66ed2232498bba04d1c3285aa60e4b85bbeb7ed
Bruce Howard <howard@fnal.gov>
2022-02-16 14:04:59 -0600

    Switch to using braces

--- sbnanaobj/StandardRecord/SRSlice.h
+++ sbnanaobj/StandardRecord/SRSlice.h
@@ -28,10 +28,10 @@ namespace caf
       SRSlice();
       virtual ~SRSlice();
 
-      unsigned producer = UINT_MAX;     ///< Index of the producer that produced this object. 
-                                        ///< In ICARUS, this is the same as the cryostat.
-      float    charge = kSignalingNaN;  ///< Calorimetric energy
-      SRVector3D vertex;                ///< Candidate neutrino vertex in local detector coordinates [cm]
+      unsigned producer { UINT_MAX };     ///< Index of the producer that produced this object. 
+                                          ///< In ICARUS, this is the same as the cryostat.
+      float    charge { kSignalingNaN };  ///< Calorimetric energy
+      SRVector3D vertex;                  ///< Candidate neutrino vertex in local detector coordinates [cm]
 
       SRTrueInteraction truth; //!< Truth information on the slice
       SRTruthMatch tmatch; //!< Matching information between truth and reco objects
@@ -42,14 +42,14 @@ namespace caf
 
       SRFakeReco fake_reco;
 
-      bool is_clear_cosmic = false;         //!< Whether pandora marks the slice as a "clear" cosmic
-      int nu_pdg           = INT_MIN;       //!< PDG assigned to the PFParticle Neutrino
-      float nu_score       = kSignalingNaN; //!< Score of how neutrino-like the slice is
+      bool is_clear_cosmic { false };         //!< Whether pandora marks the slice as a "clear" cosmic
+      int nu_pdg           { INT_MIN };       //!< PDG assigned to the PFParticle Neutrino
+      float nu_score       { kSignalingNaN }; //!< Score of how neutrino-like the slice is
 
       SRNuID nuid; //!< Neutrino ID Features (BDT inputs) going into nu_score calculation
 
-      std::vector<size_t> primary;           //!< ID's of primary tracks and showers in slice
-      int                 self = INT_MIN;    //!< ID of the particle representing this slice
+      std::vector<size_t> primary;             //!< ID's of primary tracks and showers in slice
+      int                 self { INT_MIN };    //!< ID of the particle representing this slice
 
       SRSliceRecoBranch   reco; //!< TPC reco information for the slice
 
