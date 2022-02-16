2da4564d83060de29f0c2e442e42986aaef1729c
Bruce Howard <howard@fnal.gov>
2022-02-16 13:42:19 -0600

    Address comments in PR 44 and partly introduce new SRHelper with kSignalingNaN to centralize the signaling NaN (right now just used in SRSlice and SRNuID), could be extended further.

--- sbnanaobj/StandardRecord/SRNuID.h
+++ sbnanaobj/StandardRecord/SRNuID.h
@@ -1,10 +1,12 @@
 ////////////////////////////////////////////////////////////////////////
 // \file    SRNuID.h
 // \brief   SRNuID object for Neutrino ID score features (MVA inputs). This SR code copied/based on other SR objects.
-// \author  $Author: howard@fnal.gov
+// \author  howard@fnal.gov
 ////////////////////////////////////////////////////////////////////////
-#ifndef SRNUID_H
-#define SRNUID_H
+#ifndef SBNANAOBJ_STANDARDRECORD_SRNUID_H
+#define SBNANAOBJ_STANDARDRECORD_SRNUID_H
+
+#include "sbnanaobj/StandardRecord/SRHelper.h"
 
 namespace caf
 {
@@ -13,22 +15,21 @@ namespace caf
   {
   public:
     SRNuID();
-    virtual ~SRNuID();
 
-    float nufspfos;         //!< NuNFinalStatePfos feature in NeutrinoID
-    int   nutothits;        //!< NuNHitsTotal feature in NeutrinoID
-    float nuvtxy;           //!< NuVertexY feature in NeutrinoID
-    float nuwgtdirz;        //!< NuWeightedDirZ feature in NeutrinoID
-    float nusps;            //!< NuNSpacePointsInSphere feature in NeutrinoID
-    float nueigen;          //!< NuEigenRatioInSphere feature in NeutrinoID
-    float crlongtrkdiry;    //!< CRLongestTrackDirY feature in NeutrinoID
-    float crlongtrkdef;     //!< CRLongestTrackDeflection feature in NeutrinoID
-    float crlongtrkhitfrac; //!< CRFracHitsInLongestTrack feature in NeutrinoID
-    int   crmaxhits;        //!< CRNHitsMax feature in NeutrinoID
+    float nufspfos         = kSignalingNaN; //!< NuNFinalStatePfos feature in NeutrinoID
+    int   nutothits        = -1;            //!< NuNHitsTotal feature in NeutrinoID
+    float nuvtxy           = kSignalingNaN; //!< NuVertexY feature in NeutrinoID
+    float nuwgtdirz        = kSignalingNaN; //!< NuWeightedDirZ feature in NeutrinoID
+    float nusps            = kSignalingNaN; //!< NuNSpacePointsInSphere feature in NeutrinoID
+    float nueigen          = kSignalingNaN; //!< NuEigenRatioInSphere feature in NeutrinoID
+    float crlongtrkdiry    = kSignalingNaN; //!< CRLongestTrackDirY feature in NeutrinoID
+    float crlongtrkdef     = kSignalingNaN; //!< CRLongestTrackDeflection feature in NeutrinoID
+    float crlongtrkhitfrac = kSignalingNaN; //!< CRFracHitsInLongestTrack feature in NeutrinoID
+    int   crmaxhits        = -1;            //!< CRNHitsMax feature in NeutrinoID
 
     void setDefault();
   };
 } // end namespace
 
-#endif // SRNUID_H
+#endif // SBNANAOBJ_STANDARDRECORD_SRNUID_H
 //////////////////////////////////////////////////////////////////////////////
