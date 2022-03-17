11ca0e73161b53d142212e296645f87d52f814f8
Henry Lay <h.lay@lancaster.ac.uk>
2022-03-17 10:36:05 -0500

    Add variable descriptions

--- sbnanaobj/StandardRecord/SRCRUMBSResult.h
+++ sbnanaobj/StandardRecord/SRCRUMBSResult.h
@@ -11,25 +11,25 @@ class SRCRUMBSResult {
   SRCRUMBSResult();
   ~SRCRUMBSResult() {}
 
-  float score;
-  float tpc_CRFracHitsInLongestTrack;
-  float tpc_CRLongestTrackDeflection;
-  float tpc_CRLongestTrackDirY;
-  int   tpc_CRNHitsMax;
-  float tpc_NuEigenRatioInSphere;
-  int   tpc_NuNFinalStatePfos;
-  int   tpc_NuNHitsTotal;
-  int   tpc_NuNSpacePointsInSphere;
-  float tpc_NuVertexY;
-  float tpc_NuWeightedDirZ;
-  float tpc_StoppingChi2CosmicRatio;
-  float pds_FMTotalScore;
-  float pds_FMPE;
-  float pds_FMTime;
-  float crt_TrackScore;
-  float crt_HitScore;
-  float crt_TrackTime;
-  float crt_HitTime;
+  float score;                         //!< CRUMBS result
+  float tpc_CRFracHitsInLongestTrack;  //!< fraction of slice’s space points in longest track (cosmic reco)
+  float tpc_CRLongestTrackDeflection;  //!< 1 - the cosine of the angle between the starting and finishing directions of the longest track (cosmic reco)
+  float tpc_CRLongestTrackDirY;        //!< relative direction of the longest track in Y (cosmic reco)
+  int   tpc_CRNHitsMax;                //!< the number of space points in the largest pfp
+  float tpc_NuEigenRatioInSphere;      //!< the ratio between the first and second eigenvalues from a PCA of spacepoints within 10cm of the vertex (nu reco)
+  int   tpc_NuNFinalStatePfos;         //!< the number of final state pfos (nu reco)
+  int   tpc_NuNHitsTotal;              //!< the total number of space points (nu reco)
+  int   tpc_NuNSpacePointsInSphere;    //!< the total number of space points within 10cm of the vertex (nu reco)
+  float tpc_NuVertexY;                 //!< the vertex position in Y (nu reco) [cm]
+  float tpc_NuWeightedDirZ;            //!< the Z component of the space-point weighted direction of the final state pfos (nu reco)
+  float tpc_StoppingChi2CosmicRatio;   //!< a ratio of chi2 values intended to find Bragg peaks in stopping muon tracks
+  float pds_FMTotalScore;              //!< the total flash match score
+  float pds_FMPE;                      //!< the total number of photoelectrons in the associated flash
+  float pds_FMTime;                    //!< the time associated with the flash [us]
+  float crt_TrackScore;                //!< a combination of the DCA and angle between the best matched TPC & CRT tracks
+  float crt_HitScore;                  //!< the best distance from an extrapolated TPC track to a CRT hit [cm]
+  float crt_TrackTime;                 //!< the time associated with the matched CRT track [us]
+  float crt_HitTime;                   //!< the time associated with the matched CRT hit [us]
 };
 
 } // end namespace
