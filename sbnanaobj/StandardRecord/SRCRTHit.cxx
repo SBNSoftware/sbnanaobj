b511f197430f84d878a65cf7691522d27f382de7
Henry Lay <h.lay@lancaster.ac.uk>
2022-04-18 09:36:36 -0500

    CRT hit time initialisations

--- sbnanaobj/StandardRecord/SRCRTHit.cxx
+++ sbnanaobj/StandardRecord/SRCRTHit.cxx
@@ -11,6 +11,8 @@ namespace caf
 
   SRCRTHit::SRCRTHit():
     time(std::numeric_limits<float>::signaling_NaN()),
+    t0(std::numeric_limits<float>::signaling_NaN()),
+    t1(std::numeric_limits<float>::signaling_NaN()),
     pe(std::numeric_limits<float>::signaling_NaN()),
     plane(INT_MIN)
   {}
