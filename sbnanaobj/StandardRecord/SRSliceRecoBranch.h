162c65abe7ed1edff6baef92855031532ea68edd
Ryan Howell <rhowell@icarusgpvm02.fnal.gov>
2022-05-02 19:14:18 -0500

    added unordered_maps to be filled

--- sbnanaobj/StandardRecord/SRSliceRecoBranch.h
+++ sbnanaobj/StandardRecord/SRSliceRecoBranch.h
@@ -21,15 +21,18 @@ namespace caf
     ~SRSliceRecoBranch();
             
     std::vector<SRTrack>  trk;     ///< Vector of pandora tracks
+    std::unordered_map<int, std:vector<SRTrack>> trkMap;
     size_t               ntrk;     ///< Number of panora tracks
 
     std::vector<SRShower> shw;     ///< Vector of trac showers
+    std::unordered_map<int, std:vector<SRShower>> shwMap;
     size_t               nshw;     ///< Number of trac showers
 
     std::vector<SRHit> hit;        ///< Vector of hits
     size_t            nhit;        ///< Number of hits
 
     std::vector<SRStub> stub;      ///< Vector of stubs
+    std::unordered_map<int, std:vector<SRStub>> stubMap;
     size_t             nstub;      ///< Number of stubs
 
     void fillSizes();
