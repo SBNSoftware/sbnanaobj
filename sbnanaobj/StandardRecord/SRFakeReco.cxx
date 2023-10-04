6da265af98279bd1392fd322ab0ca8b14ac3c7b6
Rhiannon Jones <rjones0211@hotmail.com>
2023-10-04 07:11:53 -0500

    Added counting parameters to SR

--- sbnanaobj/StandardRecord/SRFakeReco.cxx
+++ sbnanaobj/StandardRecord/SRFakeReco.cxx
@@ -13,6 +13,10 @@ namespace caf
   SRFakeReco::SRFakeReco():
     nuE(std::numeric_limits<float>::signaling_NaN()),
     nhad(0),
+    npion(0),
+    npi0(0),
+    nkaon(0),
+    nproton(0),
     wgt(std::numeric_limits<float>::signaling_NaN()),
     filled(false)
   {  }
--- sbnanaobj/StandardRecord/SRFakeReco.h
+++ sbnanaobj/StandardRecord/SRFakeReco.h
@@ -23,6 +23,10 @@ namespace caf
     SRFakeRecoParticle lepton; ///< Fake-reco lepton information
     std::vector<SRFakeRecoParticle> hadrons; ///< Fake-reco information on hadronic state
     int nhad; ///< Number of hadrons
+    int npion; ///< Number of charged pions
+    int npi0; ///< Number of neutral pions
+    int nkaon; ///< Number of charged kaons
+    int nproton; ///< Number of protons
     float wgt; ///< Weight for this interaction
     bool filled;
   };
