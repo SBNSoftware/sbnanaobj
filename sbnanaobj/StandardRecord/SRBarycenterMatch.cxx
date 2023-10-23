8b293dbc62ea1b8322d7db363b5140b03ec263ef
Jack Smedley <jsmedley@fnal.gov>
2023-10-23 10:19:17 -0500

    Only builds if no class version or checkum is given, need to understand this

---
+++ sbnanaobj/StandardRecord/SRBarycenterMatch.cxx
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////
+// \file    SRBarycenterMatch.cxx
+// \brief   Slice-level match info for barycenter flash matching, including best match OpFlash and matching to triggger
+// \author  jsmedley@fnal.gov
+////////////////////////////////////////////////////////////////////////
+
+#include "sbnanaobj/StandardRecord/SRBarycenterMatch.h"
+
+#include <climits>
+
+namespace caf
+{
+
+  SRBarycenterMatch::SRBarycenterMatch()
+  {}
+
+  void SRBarycenterMatch::setDefault()
+  {
+
+    chargeTotal        =                             -9999.;
+    chargeCenterXLocal =                             -9999.;
+    chargeCenter       = SRVector3D(-9999., -9999., -9999.);
+    chargeWidth        = SRVector3D(-9999., -9999., -9999.); 
+
+    flashTime          =                             -9999.;
+    flashFirstHit      =                             -9999.;
+    flashAsymmetry     =                             -9999.;
+
+    deltaY             =                             -9999.;
+    deltaZ             =                             -9999.;
+    radius             =                             -9999.;
+    deltaT             =                             -9999.;
+    overlapY           =                             -9999.;
+    overlapZ           =                             -9999.;
+    deltaZ_Trigger     =                             -9999.;
+
+  }
+
+} // end namespace caf
+////////////////////////////////////////////////////////////////////////
