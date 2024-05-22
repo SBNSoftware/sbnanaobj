a28b2f3010b31544b1d675381cf0fe89195d388f
Bruce Howard <howard@fnal.gov>
2024-05-22 16:48:01 -0500

    Incorporate Gianluca P's suggestions from PR 126 for the NuMI reprocessing release, but in this case for the develop PR

--- sbnanaobj/StandardRecord/SRTrigger.h
+++ sbnanaobj/StandardRecord/SRTrigger.h
@@ -15,16 +15,16 @@ namespace caf
     double beam_gate_det_time; ///< Beam gate time on detector time scale [us]
     double global_trigger_det_time; ///< Trigger time on detector time scale [us]
 
-    u_int64_t prev_global_trigger_time; ///< Time of the last trigger in this stream in UTC [ns]
-    int source_type; ///< gate source type
-    int trigger_type; ///< trigger type
+    std::uint64_t prev_global_trigger_time; ///< Time of the last trigger in this stream in UTC [ns]
+    unsigned int source_type; ///< gate source type
+    unsigned int trigger_type; ///< trigger type
 
     unsigned trigger_id; ///< global ID of the trigger 
     unsigned gate_id; ///< global ID of the gate
 
     unsigned trigger_count; ///< count of trigger in this stream
     unsigned gate_count; ///< count of gate in this stream
-    int gate_delta; ///< Number of gates since previous trigger in this stream
+    int gate_delta; ///< Number of gates since previous trigger in this stream (gate of this event included)
     
   };
 }
