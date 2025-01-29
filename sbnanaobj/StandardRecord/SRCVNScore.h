36409c72ae28f63b7630e2a99cc684d79ec43c7f
Tingjun Yang <tjyang@fnal.gov>
2025-01-29 13:45:52 -0600

    Update sbnanaobj/StandardRecord/SRCVNScore.h
    Co-authored-by: Gianluca Petrillo <petrillo.at.work@gmail.com>

--- sbnanaobj/StandardRecord/SRCVNScore.h
+++ sbnanaobj/StandardRecord/SRCVNScore.h
@@ -1,13 +1,20 @@
 /**
- * @brief A struct to store the four CVN scores for a single Pandora slice.
- * 
- * Each score represents a probability between 0 and 1. The sum of the four scores is always 1.
+ * @file   sbnanaobj/StandardRecord/SRCVNScore.h
+ * @brief  Defines data structures for CVN score (header only).
+ * @author Tingjun Yang
+ * @date   January 28, 2025
  */
+
 #ifndef SBNANAOBJ_STANDARDRECORD_SRCVNSCORE_H
 #define SBNANAOBJ_STANDARDRECORD_SRCVNSCORE_H
 
 namespace caf {
 
+  /**
+   * @brief A struct to store the four CVN scores for a single Pandora slice.
+   * 
+   * Each score represents a probability between 0 and 1. The sum of the four scores is always 1.
+   */
   struct SRCVNScore {
     
     static constexpr float NoScore = std::numeric_limits<float>::signaling_NaN();
