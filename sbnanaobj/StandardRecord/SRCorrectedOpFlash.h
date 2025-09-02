7b60034e041bbfb8fd4b3c3dbaefdee30c45dae3
Alejandro Sánchez Castillo <asanchezcastillo@ugr.es>
2025-09-02 10:46:31 -0500

    Add SRCorrectedOpFlash and associate it to SRSlice

---
+++ sbnanaobj/StandardRecord/SRCorrectedOpFlash.h
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////
+// \file    SRCorrectedOpFlash.h
+// \brief   Slice-level match info for corrected opflash, which includes corrected opflash time using tpc information
+// \author  acastill@fnal.gov
+////////////////////////////////////////////////////////////////////////
+#ifndef SRCORRECTEDOPFLASH_H
+#define SRCORRECTEDOPFLASH_H
+
+#include "sbnanaobj/StandardRecord/SRConstants.h"
+#include <vector>
+
+namespace caf
+{
+  class SRCorrectedOpFlash
+  {
+  public:
+
+    /// @name Data members related to the opflash corrected time
+    /// @{
+    float        OpFlashT0             { -9999. };                   ///< | OpFlash Time wrt RWM time | (ns)
+    float        UpstreamTime_lightonly             {  -9999.  };      ///< | Nu upstream wall time reconstructed using light only | (ns)
+    float        UpstreamTime_tpczcorr             {  -9999.  };       ///< | Nu upstream wall time reconstructed using light and Z from tpc vertex | (ns)
+    float        UpstreamTime_propcorr_tpczcorr    {  -9999.  };       ///< | Nu upstream wall time reconstructed using light propagation correction from tpc information and z correction from tpc vertex | (ns)
+    /// @}
+
+    void setDefault();
+
+  };
+
+} // end namespace
+
+#endif // SRCORRECTEDOPFLASH_H
+//////////////////////////////////////////////////////////////////////////////
+
