e16af823a467af7fc29e0809fcfe5f99c9943d36
justinjmueller <justin.mueller@colostate.edu>
2025-09-08 13:52:07 -0500

    Remove (suspected) useless "using" statements for reco and truth interactions

--- sbnanaobj/StandardRecord/Proxy/EpilogFwd.h
+++ sbnanaobj/StandardRecord/Proxy/EpilogFwd.h
@@ -1,6 +1,4 @@
 namespace caf{
   using SRSliceProxy = caf::Proxy<caf::SRSlice>;
   using SRSpillProxy = caf::Proxy<caf::StandardRecord>;
-  using SRInteractionDLPProxy = caf::Proxy<caf::SRInteractionDLP>;
-  using SRInteractionTruthDLPProxy = caf::Proxy<caf::SRInteractionTruthDLP>;
 }
