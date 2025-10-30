9c719063006cdb767bff151e6379d7b0c6e79eca
Jacob McLaughlin <mclaughlinjacob1996@gmail.com>
2025-10-30 08:53:47 -0500

    Putting blip entry in standard record class

--- sbnanaobj/StandardRecord/StandardRecord.h
+++ sbnanaobj/StandardRecord/StandardRecord.h
@@ -23,6 +23,7 @@
 #include "sbnanaobj/StandardRecord/SRTrigger.h"
 #include "sbnanaobj/StandardRecord/SRInteractionDLP.h"
 #include "sbnanaobj/StandardRecord/SRInteractionTruthDLP.h"
+#include "sbnanaobj/StandardRecord/SRBlip.h"
 
 /// Common Analysis Files
 namespace caf
@@ -64,6 +65,7 @@ namespace caf
     bool pass_flashtrig = false;     ///< Whether this Record passed the Flash Trigger requirement
     SRSBNDFrameShiftInfo sbnd_frames; ///< List of Frame Shift in event in unit [ns] (SBND)
     SRSBNDTimingInfo sbnd_timings; ///< List of Timing Info in event in UNIX timestamp format(SBND)
+    std::vector<SRBlip>        blips; ///Blips are small O(mm) depositions in LAr, made from recob::hits
 
     int                                ndlp = 0;       ///< Number of reco DLP (ML) interactions.
     std::vector<SRInteractionDLP>      dlp;        ///< Reco DLP (ML) interactions.  
