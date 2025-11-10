ff18ff58024de6cd87f47a41c78136d0ecffea28
kjplows <kom.plows@gmail.com>
2025-11-10 11:08:39 -0600

    Update checksum and fix merge typo

--- sbnanaobj/StandardRecord/SRTrigger.cxx
+++ sbnanaobj/StandardRecord/SRTrigger.cxx
@@ -21,7 +21,7 @@ namespace caf
     gate_delta(-1),
     trigger_logic_bits(std::numeric_limits<unsigned int>::max()),
     gate_to_trigger_time(std::numeric_limits<float>::signaling_NaN()),
-    trigger_cryo_source(std::numeric_limits<unsigned int>::max())
+    trigger_cryo_source(std::numeric_limits<unsigned int>::max()),
     passed_trigger(false),
     num_pairs_over_threshold(-1),
     monpulses_flat(),
